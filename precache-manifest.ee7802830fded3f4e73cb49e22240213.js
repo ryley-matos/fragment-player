b912f030ab53de36c46e37fc519847d1dbe35864
ryley-matos <mail@ryley.io>
2020-09-08 11:15:39 -0700

    Updates

---
+++ precache-manifest.ee7802830fded3f4e73cb49e22240213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201c8cdca9bf627ea7a101951f5dbc3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c15d3dccd7a6a7b317b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "76d4225a4c2f8d883a6c",
+    "url": "./static/js/2.8ba8687d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8ba8687d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c15d3dccd7a6a7b317b",
+    "url": "./static/js/main.b969e2ee.chunk.js"
+  },
+  {
+    "revision": "9eb19afe055e18bf662d",
+    "url": "./static/js/runtime-main.a0b4e2dc.js"
+  }
+]);
